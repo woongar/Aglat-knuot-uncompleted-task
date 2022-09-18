80a3c1ddc3d9548073bc6572c1d500c00d153050
Moshe <woongaroy@gmail.com>
2022-09-18 13:28:14 +0300

    עגלת קניות בבנייה

---
+++ javascript.js
@@ -0,0 +1,90 @@
+
+// function addToChart() {
+//     const newPar = document.createElement('p');
+//     newPar.setAttribute("id", "p1");
+//     const newContent = document.createTextNode("דג זהב נוסף" + " 10 שקל");
+//     newPar.appendChild(newContent);
+//     document.getElementById('chart').appendChild(newPar);
+//   }
+  
+
+
+// function deleteFromChart() {
+//   const deleting = document.getElementById("p1");
+//   deleting.remove();
+// }
+
+
+// let products = document.getElementsByClassName('box');
+// for(let i=0; i<products.length; i++) {
+//   if(products[1].value == products[0].value) {
+    
+//     products[1].value =  "";
+//   } else {
+//     console.log('no');
+//   }
+// }
+// console.log(products[1]);
+
+// }
+
+
+
+
+ 
+
+
+
+// function addToChart() {
+//   const newparf = document.createElement(htmlTag);
+//   const e = document.createElement("div");
+//   e.innerHTML = ("java script dom")
+//   document.body.appendChild(e);
+//   let textnode = document.createTextNode("java script dom");
+//   e.appendChild(textnode); 
+
+function addToChart() {
+
+      let change = document.getElementById("change");
+      let products = document.getElementsByClassName("box");
+      if(change.onclick == true) {
+      let newPar = document.createElement('p');
+          newPar.setAttribute("id", "p1");
+          const newContent = document.createTextNode("דג זהב נוסף" + " 10 שקל")
+          newPar.appendChild(newContent);
+          document.getElementById('chart').appendChild(newPar);
+          // } else {
+          //   newPar="";
+          // }
+        }  
+      }
+// } else if(products[1]) {
+//        console.log("yey");
+// }
+
+
+function deleteFromChart() {
+  const deleting = document.getElementById("p1");
+  deleting.remove();
+}
+
+  
+
+
+//     let products = document.getElementsByClassName('box') 
+// 
+//     if(products[1].value == products[0].value) {const newContent = document.createTextNode("דג דיסקוס  נוסף" + " 70 שקל");
+
+
+// const newPar = document.createElement('p');
+//  const newContent = document.createTextNode("דג זהב נוסף" + " 10 שקל");
+//  newPar.appendChild(newContent);
+//  document.getElementById('chart').appendChild(newPar);for(let i=0; i<products.length; i++) {
+
+
+  //   products[0].style.color('keyup',() => {
+  //     console.log("its ok")
+  //   })
+  // }
+
+  // products[0].style.backgroundColor="red";
